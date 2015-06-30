35b789620e56a0380598f0435f100efc41eb4520
baoerpoo <baoerpoo@baoerpoodeMacBook-Pro.local>
2015-06-30 17:13:30 +0800

    Initial Commit

---
+++ Flappybird/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Flappybird
+//
+//  Created by baoerpoo on 15/6/30.
+//  Copyright (c) 2015年 baoerpoo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
